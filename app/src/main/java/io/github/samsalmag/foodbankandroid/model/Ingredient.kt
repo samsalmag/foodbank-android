5ccaab2e898405a55588a612b28df1ad96e4ce5a
Sam Salek <33421876+sam-salek@users.noreply.github.com>
2024-09-25 21:51:42 +0200

    Renamed variable. Added new function.

--- app/src/main/java/io/github/samsalmag/foodbankandroid/model/Ingredient.kt
+++ app/src/main/java/io/github/samsalmag/foodbankandroid/model/Ingredient.kt
@@ -2,8 +2,12 @@ package io.github.samsalmag.foodbankandroid.model
 
 data class Ingredient (
     var name: String,
-    var amount: Int,
+    var quantity: Int,
     var unit: String
 ) {
     constructor(): this("", 0, "")
+
+    fun canStack(other: Ingredient): Boolean {
+        return name == other.name && unit == other.unit
+    }
 }
