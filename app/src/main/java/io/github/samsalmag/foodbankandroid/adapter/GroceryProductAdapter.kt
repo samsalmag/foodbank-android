f931d220c04fd4e0ee16a9d1c00aaafaa586237a
Sam Salek <33421876+sam-salek@users.noreply.github.com>
2024-09-29 15:37:27 +0200

    Cleaned up and organized code in GroceryProductAdapter

--- app/src/main/java/io/github/samsalmag/foodbankandroid/adapter/GroceryProductAdapter.kt
+++ app/src/main/java/io/github/samsalmag/foodbankandroid/adapter/GroceryProductAdapter.kt
@@ -15,9 +15,9 @@ import java.util.logging.Logger
 class GroceryProductAdapter(
     private val groceryProducts: MutableList<GroceryProduct>,
     private var checkboxClickListener: CheckboxClickListener? = null
-) : RecyclerView.Adapter<GroceryProductAdapter.GroceryItemViewHolder>() {
+) : RecyclerView.Adapter<GroceryProductAdapter.GroceryProductViewHolder>() {
 
-    class GroceryItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    class GroceryProductViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val ingredientName: TextView = itemView.findViewById(R.id.textView_ingredientName)
         val ingredientQuantity: TextView = itemView.findViewById(R.id.textView_ingredientQuantity)
         val ingredientUnit: TextView = itemView.findViewById(R.id.textView_ingredientUnit)
@@ -31,36 +31,20 @@ class GroceryProductAdapter(
 
     private val LOGGER = Logger.getLogger(GroceryProductAdapter::class.java.name)
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GroceryItemViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GroceryProductViewHolder {
         val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_grocery_product, parent, false)
-        return GroceryItemViewHolder(itemView)
+        return GroceryProductViewHolder(itemView)
     }
 
-    override fun onBindViewHolder(holder: GroceryItemViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: GroceryProductViewHolder, position: Int) {
         val groceryProduct = groceryProducts[position]
         holder.ingredientName.text = groceryProduct.ingredient.name
         holder.ingredientQuantity.text = groceryProduct.ingredient.quantity.toString()
         holder.ingredientUnit.text = groceryProduct.ingredient.unit
         holder.groceryComplete.isChecked = groceryProduct.isComplete
 
-        // Alternate background color
-        if (position % 2 == 0)
-            holder.itemView.setBackgroundColor(ContextCompat.getColor(holder.itemView.context, R.color.recycler_view_even_item))
-        else
-            holder.itemView.setBackgroundColor(ContextCompat.getColor(holder.itemView.context, R.color.recycler_view_odd_item))
-
-        // Init overlay visibility
-        holder.completeOverlay.visibility = if (holder.groceryComplete.isChecked) View.VISIBLE else View.GONE
-
-        // Handle checkbox click event
-        holder.groceryComplete.setOnCheckedChangeListener { _, isChecked ->
-            groceryProduct.isComplete = isChecked
-            groceryProducts[position] = groceryProduct  // Update the grocery product in the list
-            SharedPreferencesUtil.saveGroceryList(holder.itemView.context, groceryProducts)  // Save the updated list
-
-            holder.completeOverlay.visibility = if (isChecked) View.VISIBLE else View.GONE  // Update overlay visibility
-            checkboxClickListener?.onCheckboxClick(position, isChecked) // If a listener is set, notify it
-        }
+        initViewAppearance(holder, position)
+        initCheckboxIngredientComplete(holder, position)
     }
 
     override fun getItemCount(): Int {
@@ -71,5 +55,38 @@ class GroceryProductAdapter(
         groceryProducts.clear()
         groceryProducts.addAll(newProducts)
         notifyDataSetChanged()
+    }
+
+    /**
+     * Initializes the view's appearance by setting the background color and 'completion' overlay visibility.
+     *
+     * @param holder The ViewHolder for the grocery product.
+     * @param position The position of the grocery product in the list.
+     */
+    private fun initViewAppearance(holder: GroceryProductViewHolder, position: Int) {
+        // Alternate background color
+        if (position % 2 == 0)
+            holder.itemView.setBackgroundColor(ContextCompat.getColor(holder.itemView.context, R.color.recycler_view_even_item))
+        else
+            holder.itemView.setBackgroundColor(ContextCompat.getColor(holder.itemView.context, R.color.recycler_view_odd_item))
+
+        // Init overlay visibility
+       updateCompletionOverlay(holder, holder.groceryComplete.isChecked)
+    }
+
+    private fun initCheckboxIngredientComplete(holder: GroceryProductViewHolder, position: Int) {
+        // Handle checkbox click event
+        holder.groceryComplete.setOnCheckedChangeListener { _, isChecked ->
+            val groceryProduct = groceryProducts[position]
+            groceryProduct.isComplete = isChecked   // Update the 'isComplete' flag
+            SharedPreferencesUtil.saveGroceryList(holder.itemView.context, groceryProducts)  // Save the updated list
+
+            updateCompletionOverlay(holder, isChecked)
+            checkboxClickListener?.onCheckboxClick(position, isChecked) // If a listener is set, notify it
+        }
+    }
+
+    private fun updateCompletionOverlay(holder: GroceryProductViewHolder, isChecked: Boolean) {
+        holder.completeOverlay.visibility = if (isChecked) View.VISIBLE else View.GONE
     }
 }
