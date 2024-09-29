f66500c4e0f7dcd614d571ac413cc7c803d806ac
Sam Salek <33421876+sam-salek@users.noreply.github.com>
2024-09-29 21:08:01 +0200

    Some setup for eventual testing

--- app/src/androidTest/java/io/github/samsalmag/foodbankandroid/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/io/github/samsalmag/foodbankandroid/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package io.samsalmag.foodbankandroid
+package io.github.samsalmag.foodbankandroid
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
---
+++ app/src/androidTest/java/io/github/samsalmag/foodbankandroid/fragment/GroceryListFragmentTest.kt
@@ -0,0 +1,5 @@
+package io.github.samsalmag.foodbankandroid.fragment
+
+class GroceryListFragmentTest {
+
+}
